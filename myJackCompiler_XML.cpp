eaf12112f008536241c9371e8b16950a0b06294b
SkyrimYun <skyrim-yy@outlook.com>
2020-03-07 17:12:26 +0100

    finish Tokenizer

--- myJackCompiler_XML.cpp
+++ myJackCompiler_XML.cpp
@@ -9,17 +9,93 @@
 
 using namespace std;
 
+#define KEYWORD 0
+#define SYMBOL 1
+#define IDENTIFIER 2
+#define INT_CONST 3
+#define STRING_CONST 4
+#define CLASS 5
+#define METHOD 6
+#define FUNCTION 7
+#define CONSTRUCTOR 8
+#define INT 9
+#define BOOLEAN 10
+#define CHAR 11
+#define VOID 12
+#define VAR 13
+#define STATIC 14
+#define FIELD 15
+#define LET 16
+#define DO 17
+#define IF 18
+#define ELSE 19
+#define WHILE 20
+#define RETURN 21
+#define TRUE 22
+#define FALSE 23
+#define NULL 24
+#define THIS 25
+#define INVALID 26
+
 class JackTokenizer
 {
 private:
-    ifstream ist;
+    struct Token
+    {
+        string val;
+        int type;
+
+        Token(){};
+        void set(string &s, int t)
+        {
+            val = s;
+            type = t;
+        }
+        void reset()
+        {
+            val = "";
+            type = -1;
+        }
+    };
+
     string fileBuffer;
+    Token curToken;
+    int index = 0;
+    vector<string> keywords{
+        "class",
+        "method",
+        "function",
+        "constructor",
+        "int",
+        "boolean",
+        "char",
+        "void",
+        "var",
+        "static",
+        "field",
+        "let",
+        "do",
+        "if",
+        "else",
+        "while",
+        "return",
+        "true",
+        "false",
+        "null",
+        "this",
+    };
+    vector<char> symbolList{'{', '}', '(', ')', '[', ']', '.', ',', '.', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '_', '~'};
 
     string removeLineBlank(string &);
     string removeComments(string &);
+    bool isSymbol(char);
+    bool isKeyword(string &);
+    char getNextCharacter();
+    void unget();
 
 public:
     JackTokenizer(string &);
+    JackTokenizer(){};
 
     //are there more tokens in the input
     bool hasMoreTokens();
@@ -92,9 +168,41 @@ string JackTokenizer::removeComments(string &file)
     return s;
 }
 
+char JackTokenizer::getNextCharacter()
+{
+    char c = fileBuffer[index];
+    index++;
+    return c;
+}
+
+void JackTokenizer::unget()
+{
+    index--;
+}
+
+bool JackTokenizer::isSymbol(char c)
+{
+    for (char s : symbolList)
+    {
+        if (c == s)
+            return true;
+    }
+    return false;
+}
+
+bool JackTokenizer::isKeyword(string &s)
+{
+    for (string ss : keywords)
+    {
+        if (s == ss)
+            return true;
+    }
+    return false;
+}
+
 JackTokenizer::JackTokenizer(string &path)
 {
-    ist = ifstream(path.c_str());
+    ifstream ist = ifstream(path.c_str());
 
     if (!ist)
         throw runtime_error("cannot open input file");
@@ -109,11 +217,131 @@ JackTokenizer::JackTokenizer(string &path)
     }
 
     fileBuffer = removeComments(fileBuffer);
-    cout << fileBuffer << endl;
+    //cout << fileBuffer << endl;
+    advance();
+}
+
+bool JackTokenizer::hasMoreTokens()
+{
+    return index < fileBuffer.size();
+}
+
+void JackTokenizer::advance()
+{
+    if (hasMoreTokens)
+    {
+        char c = getNextCharacter();
+        string curValue;
+
+        //handle string constant
+        if (c == '"')
+        {
+            curValue += c;
+            do
+            {
+                c = getNextCharacter();
+                curValue += c;
+            } while (c != '"');
+            curToken.set(curValue, STRING_CONST);
+        }
+        //handle keyword or indentifier
+        else if (isalpha(c))
+        {
+            while (c != ' ' && (!isSymbol(c) || c == '_'))
+            {
+                curValue += c;
+                c = getNextCharacter();
+            }
+            if (isKeyword(curValue))
+                curToken.set(curValue, KEYWORD);
+            else
+                curToken.set(curValue, IDENTIFIER);
+
+            unget();
+        }
+        //handle integer constant
+        else if (isdigit(c))
+        {
+            while (isdigit(c))
+            {
+                curValue += c;
+                c = getNextCharacter();
+            }
+            curToken.set(curValue, INT_CONST);
+
+            unget();
+        }
+        //handle symbol
+        else if (isSymbol(c))
+        {
+            curValue = c;
+            curToken.set(curValue, SYMBOL);
+        }
+        else if (isspace(c))
+        {
+            if (hasMoreTokens())
+                advance();
+        }
+        else
+        {
+            throw runtime_error("invalid input Token!");
+        }
+    }
+}
+
+int JackTokenizer::tokenType()
+{
+    return curToken.type;
+}
+
+int JackTokenizer::keyWord()
+{
+    if (curToken.type != KEYWORD)
+        throw runtime_error("current token is not a keyword!");
+    for (int i = 0; i < keywords.size(); i++)
+    {
+        if (curToken.val == keywords[i])
+            return i + 5;
+    }
+}
+
+char JackTokenizer::symbol()
+{
+    if (curToken.type != SYMBOL)
+        throw runtime_error("current token is not a symbol!");
+    return curToken.val[0];
+}
+
+string JackTokenizer::identifier()
+{
+    if (curToken.type != IDENTIFIER)
+        throw runtime_error("current token is not a identifier!");
+    return curToken.val;
+}
+
+int JackTokenizer::intVal()
+{
+    if (curToken.type != INT_CONST)
+        throw runtime_error("current token is not a integer constant!");
+    stringstream sToInt(curToken.val);
+    int ret;
+    sToInt >> ret;
+    return ret;
+}
+
+string JackTokenizer::stringVal()
+{
+    if (curToken.type != STRING_CONST)
+        throw runtime_error("current token is not a string constant!");
+    return curToken.val;
 }
 
 class CompilationEngine
 {
+private:
+    JackTokenizer tokenizer;
+    string outputPath;
+
 public:
     CompilationEngine(JackTokenizer &, string &);
 
@@ -168,6 +396,9 @@ public:
 
 CompilationEngine::CompilationEngine(JackTokenizer &jt, string &s)
 {
+    this->tokenizer = tokenizer;
+    this->outputPath = outputPath;
+    CompileClass();
 }
 
 class JackAnalyzer
